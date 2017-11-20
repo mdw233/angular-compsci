84b66d9ba0150de33aefebba54ffc7529b571ed2
Matt Withum <mdw233@gmail.com>
2017-11-19 20:50:17 -0500

    added the traverse method and all of the other methods that use traverse

--- src/app/tree/shared/services/binary-tree.service.spec.ts
+++ src/app/tree/shared/services/binary-tree.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { BinaryTreeService } from './binary-tree.service';
 import { BinaryTree } from '../models/binary-tree';
+import { BinaryTreeNode } from '../models/binary-tree-node';
 
 
 describe('BinaryTreeService', () => {
@@ -44,8 +45,55 @@ describe('BinaryTreeService', () => {
 
   it('adding should not fail', inject([BinaryTreeService], (service: BinaryTreeService) => {
     let tree = new BinaryTree<number>();
-    let result = service.add(tree, 3);
+    service.add(tree, 3);
+    let result = service.contains(tree, 3);
 
     expect(result).toBe(true);
+  }));
+  
+  it('adding via array should work', inject([BinaryTreeService], (service: BinaryTreeService) => {
+    let tree = new BinaryTree<number>();
+    service.add(tree, 7, 9, 1, 8, 12, 25, 19, 31, 4);
+    let result = service.contains(tree, 1);
+
+    expect(result).toBe(true);
+  }));
+  
+  it("should traverse the tree correctly", inject([BinaryTreeService], (service: BinaryTreeService) => {
+    let tree = new BinaryTree<number>();
+    service.add(tree, 7, 9, 1, 8, 12, 25, 19, 31, 4);
+
+    let array = new Array<number>();
+
+    service.traverse(tree, (node: BinaryTreeNode<number>) => { array.push(node.value) });
+
+    expect(array).toEqual([1,4,7,8,9,12,19,25,31]);
+  }));
+  
+  it("should print the array correctly", inject([BinaryTreeService], (service: BinaryTreeService) => {
+    let tree = new BinaryTree<number>();
+    service.add(tree, 7, 9, 1, 8, 12, 25, 19, 31, 4);
+
+    let array = service.toArray(tree);
+
+    expect(array).toEqual([1,4,7,8,9,12,19,25,31]);
+  }));
+  
+  it("should print the array string correctly", inject([BinaryTreeService], (service: BinaryTreeService) => {
+    let tree = new BinaryTree<number>();
+    service.add(tree, 7, 9, 1, 8, 12, 25, 19, 31, 4);
+
+    let array = service.toString(tree);
+
+    expect(array).toEqual("1,4,7,8,9,12,19,25,31");
+  }));
+  
+  it("should return the correct size", inject([BinaryTreeService], (service: BinaryTreeService) => {
+    let tree = new BinaryTree<number>();
+    service.add(tree, 7, 9, 1, 8, 12, 25, 19, 31, 4);
+
+    let size = service.size(tree);
+
+    expect(size).toEqual(9);
   }));
 });
--- src/app/tree/shared/services/binary-tree.service.ts
+++ src/app/tree/shared/services/binary-tree.service.ts
@@ -21,36 +21,78 @@ export class BinaryTreeService {
     return found;
   }
 
-  add<T>(tree: BinaryTree<T>, value: T): boolean {
-    let node = new BinaryTreeNode(value);
-    if (tree.root == null) {
-      tree.root = node;
-      return true;
-    }
+  add<T>(tree: BinaryTree<T>, ...values: Array<T>): void {
 
-    let current = tree.root;
+    for (let value of values) {
 
-    while (true) {
-      //when the new value is less than node value, go left young man
-      if (node.value < current.value) {
-        if (current.left == null) {
-          //if there is no left - hey that's our spot!
-          current.left = node;
-          return true;
+      let node = new BinaryTreeNode(value);
+      if (tree.root == null) {
+        tree.root = node;
+        continue;
+      }
+
+      let current = tree.root;
+
+      while (true) {
+        //when the new value is less than node value, go left young man
+        if (node.value < current.value) {
+          if (current.left == null) {
+            //if there is no left - hey that's our spot!
+            current.left = node;
+            break;
+          } else {
+            //if there is a left we need to keep going
+            current = current.left;
+          }
+        } else if (node.value > current.value) {
+          if (current.right == null) {
+            //if there is no right - well pardner, we'll take it
+            current.right = node;
+            break;
+          } else {
+            current = current.right;
+          }
         } else {
-          //if there is a left we need to keep going
-          current = current.left;
+          break; //they're equal nothing to do
         }
-      } else if (node.value > current.value) {
-        if (current.right == null) {
-          //if there is no right - well pardner, we'll take it
-          current.right = node;
-        } else {
-          current = current.right;
-        }
-      } else {
-        return false; //they're equal nothing to do
       }
     }
+  
+  }
+
+  traverse<T>(tree: BinaryTree<T>, process: (node: BinaryTreeNode<T>) => void) {
+
+    function inOrder(node: BinaryTreeNode<T>)
+    {
+      if (!node) return;
+
+      if (node.left) {
+        inOrder(node.left);
+      }
+
+      process(node);
+
+      if (node.right) {
+        inOrder(node.right);
+      }
+    }
+
+    inOrder(tree.root);
+  }
+
+  toArray<T>(tree: BinaryTree<T>): Array<T> {
+    let array = new Array<T>();
+    this.traverse(tree, (node: BinaryTreeNode<T>) => array.push(node.value));
+    return array;
+  }
+  
+  toString<T>(tree: BinaryTree<T>): string {
+    let array = this.toArray(tree);
+    return array.join(","); 
+  }
+  
+  size<T>(tree: BinaryTree<T>): number {
+    let array = this.toArray(tree);
+    return array.length; 
   }
 }
